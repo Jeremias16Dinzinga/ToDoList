a19f4f4f039fbcf83ecd8c44af8f760d9e727ac3
Jeremias16Dinzinga <jeremias.antonio1999@gmail.com>
2025-03-02 14:44:21 +0000

    Configuration(db and all structure)

---
+++ src/test/java/com/sweet_company/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweet_company.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
